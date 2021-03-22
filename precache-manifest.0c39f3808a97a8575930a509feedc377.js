a95420b2e7a83ae8ef6d1695756a27442fd21a97
SyedaMiralKazmi <80279916+SyedaMiralKazmi@users.noreply.github.com>
2021-03-22 23:23:39 +0100

    Add files via upload
    Index.HTML and Build files

---
+++ precache-manifest.0c39f3808a97a8575930a509feedc377.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9d84f1b3db03fa7473",
+    "url": "/CovidStats/static/css/main.05cb2cd1.chunk.css"
+  },
+  {
+    "revision": "2e9d84f1b3db03fa7473",
+    "url": "/CovidStats/static/js/main.94472e9d.chunk.js"
+  },
+  {
+    "revision": "876d6c19ea274f974c94",
+    "url": "/CovidStats/static/js/runtime~main.efdc124a.js"
+  },
+  {
+    "revision": "00f187ed646925325fe9",
+    "url": "/CovidStats/static/css/2.4687953a.chunk.css"
+  },
+  {
+    "revision": "00f187ed646925325fe9",
+    "url": "/CovidStats/static/js/2.286a0011.chunk.js"
+  },
+  {
+    "revision": "e71754485fc35ba7daf698857e2c58eb",
+    "url": "/CovidStats/static/media/CSLOGO.e7175448.png"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CovidStats/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CovidStats/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CovidStats/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CovidStats/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "ba45c8f60456a672e003a875e469d0eb",
+    "url": "/CovidStats/static/media/Desert.ba45c8f6.jpg"
+  },
+  {
+    "revision": "99396be59ed01da4766cb4134d7043f8",
+    "url": "/CovidStats/index.html"
+  }
+];
